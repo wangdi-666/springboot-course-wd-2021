d66a316a70a3f0f2ac0c70331fe0d8ee16433e27
wangdi <1589955459@qq.com>
2021-04-30 09:58:07 +0800

    第十四次课-Spring-init2||SpringMVC-init1

---
+++ spring-examples/src/test/java/com/example/springexamples/example05/UserService05Test.java
@@ -0,0 +1,21 @@
+package com.example.springexamples.example05;
+
+import com.example.springexamples.example05.transactions.service.UserService05;
+import com.example.springexamples.example05.transactions.entity.User05;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class UserService05Test {
+    @Autowired
+    private UserService05 userService05;
+
+    @Test
+    public void test() {
+        User05 u = User05.builder().id(79L).name("SUN").build();
+        userService05.addUser(u);
+    }
+}
